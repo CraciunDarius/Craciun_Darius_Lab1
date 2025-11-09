9f5d14ac9216bed4dd0b7a9c3a108ccde4143c06
Craciun Darius <craciun_darius@yahoo.com>
2025-11-09 23:57:15 +0200

    Add project files.

---
+++ Data/Craciun_Darius_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Craciun_Darius_Lab2.Models;
+
+namespace Craciun_Darius_Lab2.Data
+{
+    public class Craciun_Darius_Lab2Context : DbContext
+    {
+        public Craciun_Darius_Lab2Context (DbContextOptions<Craciun_Darius_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Craciun_Darius_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
