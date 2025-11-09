c5e129d2701f35ede187a1c6d49e22addd3c9e63
Craciun Darius <craciun_darius@yahoo.com>
2025-11-10 00:24:52 +0200

    Finish Lab 2

--- Pages/Publishers/Create.cshtml.cs
+++ Pages/Publishers/Create.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Craciun_Darius_Lab2.Data;
 using Craciun_Darius_Lab2.Models;
 
-namespace Craciun_Darius_Lab2.Pages.Books
+namespace Craciun_Darius_Lab2.Pages.Publishers
 {
     public class CreateModel : PageModel
     {
@@ -21,12 +21,11 @@ namespace Craciun_Darius_Lab2.Pages.Books
 
         public IActionResult OnGet()
         {
-            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
             return Page();
         }
 
         [BindProperty]
-        public Book Book { get; set; } = default!;
+        public Publisher Publisher { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
@@ -36,7 +35,7 @@ namespace Craciun_Darius_Lab2.Pages.Books
                 return Page();
             }
 
-            _context.Book.Add(Book);
+            _context.Publisher.Add(Publisher);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Craciun_Darius_Lab2.Data;
 using Craciun_Darius_Lab2.Models;
 
-namespace Craciun_Darius_Lab2.Pages.Books
+namespace Craciun_Darius_Lab2.Pages.Publishers
 {
     public class IndexModel : PageModel
     {
@@ -19,13 +19,11 @@ namespace Craciun_Darius_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        public IList<Publisher> Publisher { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Book = await _context.Book
-                .Include(b => b.Publisher)
-                .ToListAsync();
+            Publisher = await _context.Publisher.ToListAsync();
         }
     }
 }
