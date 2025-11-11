b1e96068e3f9d2a7dd3ced2e509ba06ea226132d
Craciun Darius <craciun_darius@yahoo.com>
2025-11-11 23:38:00 +0200

    Finished Lab 3

--- Models/Book.cs
+++ Models/Book.cs
@@ -19,5 +19,6 @@ namespace Craciun_Darius_Lab2.Models
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }//navigation property
-}
+        public ICollection<BookCategory>? BookCategories { get; set; }
+    }
 }
